63ac272c70521ed33a0cae92725e2b78a322b6a3
AnnaVlady <7753277av@gmail.com>
2023-02-09 16:22:48 +0300

    if_else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Кирилл")
+{
+    Console.WriteLine("Ура, это ведь Кирилл!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

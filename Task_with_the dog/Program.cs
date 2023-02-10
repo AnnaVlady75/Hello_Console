d7785c5e1a7af8daea9a46a930becac80871c1f1
AnnaVlady <7753277av@gmail.com>
2023-02-10 12:27:08 +0300

    added Task with dog

---
+++ Task_with_the dog/Program.cs
@@ -0,0 +1,24 @@
+﻿double count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed  = 2;
+int dogSpeed = 5;
+bool Friend = 2;
+
+while(distance>10)
+{    if(Friend = 1)
+{
+    time = distance / (firstFriendSpeed+dogSpeed);
+    Friend = 2;
+}
+else
+{
+    time = distance / (secondFriendSpeed+dogSpeed);
+    Friend = 1;
+}
+distance = distance - distance(firstFriendSpeed+socondFriendSpeed) * time;
+Console.Write("count = ");
+Console.WriteLine(count);
+count = count + 1;
+}
+

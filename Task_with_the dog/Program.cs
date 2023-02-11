28b29b92e386ec94c3718567a7879e2b954d8701
AnnaVlady <7753277av@gmail.com>
2023-02-11 23:15:15 +0300

    added positive nums

--- Task_with_the dog/Program.cs
+++ Task_with_the dog/Program.cs
@@ -3,10 +3,12 @@ int distance = 10000;
 int firstFriendSpeed = 1;
 int secondFriendSpeed  = 2;
 int dogSpeed = 5;
-bool Friend = 2;
+string Friend = Console.ReadLine();
+int time = Console.
 
 while(distance>10)
-{    if(Friend = 1)
+{   
+     if(Friend == 1)
 {
     time = distance / (firstFriendSpeed+dogSpeed);
     Friend = 2;
@@ -16,7 +18,7 @@ else
     time = distance / (secondFriendSpeed+dogSpeed);
     Friend = 1;
 }
-distance = distance - distance(firstFriendSpeed+socondFriendSpeed) * time;
+distance = distance - distance(firstFriendSpeed+secondFriendSpeed) * time;
 Console.Write("count = ");
 Console.WriteLine(count);
 count = count + 1;
---
+++ positive_numbers_array/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] numbers = { 3, -7, 15, -4, 23 };
+int i = 0;
+while (i < numbers.Length)
+{
+    if (numbers[i] > 0) Console.WriteLine($"{numbers[i]}, ");
+    i++;
+}

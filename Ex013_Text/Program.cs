4f023032b315714ccc26114cf2bd4453a53b4285
AnnaVlady <7753277av@gmail.com>
2023-02-20 19:10:44 +0300

    added ex014

---
+++ Ex013_Text/Program.cs
@@ -0,0 +1,37 @@
+﻿// Дан текст. Нужно все пробелы заменить черточками.
+// Маленькие буквы "к" и "с" на большие.
+
+
+string text = "Я помню чудное мгновенье:"
+            + "Передо мной явилась ты,"
+            + "Как мимолетное виденье,"
+            + "Как гений чистой красоты.";
+// string s=  "qwerty"
+//             012345
+// s[3] -> r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+            if (text[i] == oldValue)
+                result = result + $"{newValue}";
+            else result = result + $"{text[i]}";
+    }
+
+
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+
+
+
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+
+
---
+++ Methods are/Program.cs
@@ -0,0 +1,51 @@
+﻿//1 тип Метода, который ничего не возвращает и ничего не принимает
+void Method1()
+{
+    Console.WriteLine("Пишем текст ");
+}
+//Вызов метода:
+//Method1();
+
+//2 тип Метода, который ничего не возвращает, но принимает аргументы
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2(" Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+
+}
+//Method21(msg:" Текст сообщения", count: 3);
+
+
+//3 тип Метода, который возвращает, но ничего не принимает 
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+//4 тип Метода, который что-то принимает и что-то возвращает
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "q");
+Console.WriteLine(res);

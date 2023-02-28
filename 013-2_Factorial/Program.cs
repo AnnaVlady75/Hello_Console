59f02225c174f1f800dd1c0a2fa01a2e9b0edbbf
AnnaVlady <7753277av@gmail.com>
2023-02-28 16:11:03 +0300

    added ex013

---
+++ 013-2_Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿// Найти факториал
+// double Factorial(int n)
+// {
+//     // 1! = 1
+//     // 0! = 1
+//     if (n == 1) return 1;
+//     else return n* Factorial(n - 1);
+// }
+
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// Фибоначчи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1)+f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n==1||n==2) return 1;
+    else
+    {
+        return Fibonacci(n-1)+Fibonacci(n-2);
+    }
+}
+
+for (int i = 1; i < 30; i++)
+{
+    Console.WriteLine($"f{i} = {Fibonacci(i)}");
+}
---
+++ Test001/Program.cs
@@ -0,0 +1,16 @@
+﻿// int maxI = 99;
+// int minI  =-99;
+// double x = 0;
+// Random rnd = new Random();
+// for (int i = 0; i<5; i++)
+// //  0-0.1                умножение на необходимое число   
+// //  0 - 0,9              до получения нужной разрядности (верхнего предела)
+// (x = rnd.NextDouble() * (maxI - minI));   
+
+// Console.WriteLine($"{x} + {minI} = {x+minI}");
+
+// Console.WriteLine($" max-min = {maxI - minI}");
+//                                          //коррелирует результат до заданного нижнего предела
+//                                                         //  |
+// Console.WriteLine($" (max-min) + min = {maxI - minI} + {minI}");
+// Console.WriteLine($" (max-min) - min = {maxI - minI} - {minI}");
